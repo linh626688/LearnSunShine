9b59287cf9d3972788b71e4bad7e764dcc032d23
Đặng Linh <ng Linh>
2016-05-27 23:26:50 +0700

    first commit clone data from sever done

---
+++ app/src/main/java/helix/learnsunshine/MainActivity.java
@@ -0,0 +1,18 @@
+package helix.learnsunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
+        }
+    }
+
+}
